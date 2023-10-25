4d2baa8a2da8005048b7c2c1b1ec039572c4efd5
Amresh Maurya <amresh.terminal@gmail.com>
2023-10-25 22:40:13 +0530

    basic setup

---
+++ app/auth/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+const Auth = () => {
+  return (
+
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-cover">
+   <div className="bg-black w-full h-full lg:bg-opacity-50">
+    <nav className="px-12 py-5">
+        <img src="/images/logo.png" alt="logo" className="h-12" />
+    </nav>
+   </div>
+   <div className="flex justify-center">
+    .bg-black.bg-opacity-70.px-16
+   </div>
+
+
+
+    </div>
+ 
+  )
+}
+export default Auth
---
+++ components/Auth.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const Auth = () => {
+  return (
+    <>
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-cover">
+    </div>
+    </>
+  )
+}
+export default Auth
